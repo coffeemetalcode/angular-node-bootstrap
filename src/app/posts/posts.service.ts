dc504e059d52853eb3e0157eec73c39cd9b5f2b3
David B. Stocker <coffeemetalcode@gmail.com>
2021-01-19 22:06:11 -0500

    auto refresh on add post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -13,9 +14,21 @@ export class PostsService {
   constructor(private _http: HttpClient) {}
 
   getPosts() {
-    return this._http.get<{message: string, posts: Post[]}>('http://localhost:8080/api/posts')
-      .subscribe((data) => {
-        this._posts = data.posts;
+    return this._http.get<{ message: string, posts: any }>('http://localhost:8080/api/posts')
+      .pipe(map((data) => {
+        // console.log('The original data', data.posts);
+        return data.posts.map((post) => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }
+        });
+        
+      }))
+      .subscribe((posts) => {
+        // console.log('The transformed data', posts);
+        this._posts = posts;
         this._postsUpdated.next([...this._posts]);
       });
   }
@@ -24,17 +37,26 @@ export class PostsService {
     return this._postsUpdated.asObservable();
   }
 
-  addPost(id: null, title:string, content: string) {
+  addPost(id: string, title: string, content: string) {
     const post: Post = {
       id: id,
       title: title,
       content: content
     };
 
-    this._http.post<{message: string}>('http://localhost:8080/api/posts', post)
+    this._http.post<{ message: string, id: string }>('http://localhost:8080/api/posts', post)
       .subscribe((data) => {
-        console.log(data.message);
+        post.id = data.id;
         this._posts.push(post);
+        this._postsUpdated.next([...this._posts]);
+      });
+  }
+
+  deletePost(id: string) {
+    this._http.delete<{ message: string }>('http://localhost:8080/api/posts/' + id)
+      .subscribe((data) => {
+        const updatedPosts = this._posts.filter((post) => post.id !== id);
+        this._posts = updatedPosts;
         this._postsUpdated.next([...this._posts]);
       });
   }
